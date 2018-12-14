632184c7555319647c2bb02ce44c4856dbd454a3
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-14 04:40:28 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(one)
+  one
 end
 
-def greeting
-
+def greeting( one, two)
+one = "What is " 
+two = "up!"
 end
 
 def return_a_value
-
+puts "Nice"
+"Nice"
 end
 
 def last_evaluated_value
-
+me = "learning"
+return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+topping
 end
